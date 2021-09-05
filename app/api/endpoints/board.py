4ec69906e7e994c6b4f51d41b2420d0ac626e2b1
Henri Kestiö <henri.kestio@gmail.com>
2021-09-05 12:07:50 +0300

    add websocket manager and websocket endpoint for handling columns

--- app/api/endpoints/board.py
+++ app/api/endpoints/board.py
@@ -1,7 +1,10 @@
+import json
 from typing import List
 
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import WebSocket
+from fastapi import WebSocketDisconnect
 from pydantic.types import UUID4
 from starlette.responses import Response
 from starlette.status import HTTP_404_NOT_FOUND
@@ -12,9 +15,16 @@ from app.crud.board import delete_board
 from app.crud.board import get_user_board
 from app.crud.board import get_user_boards
 from app.crud.board import update_board
+from app.crud.column import create_column_and_update_board_column_order
+from app.crud.column import delete_column_and_update_board_column_order
+from app.crud.column import get_board_columns_and_column_order
+from app.crud.column import update_column_and_update_board_column_order
 from app.models.board import Board
 from app.models.board import BoardCreate
+from app.models.column import Column
+from app.models.column import ColumnCreate
 from app.models.user import User
+from app.websocket import ConnectionManager
 
 
 board_router = APIRouter()
@@ -53,3 +63,66 @@ async def delete_user_board(board_uuid: UUID4, current_user: User = Depends(get_
     delete_board_response = await delete_board(board_uuid, current_user.user_uuid)
 
     return delete_board_response
+
+
+async def get_board_data(board_uuid: UUID4):
+    board_data_records = await get_board_columns_and_column_order(board_uuid)
+
+    if board_data_records:
+        board_columns = [json.loads(Column(**column).json()) for column in board_data_records]
+
+        column_order_uuid_list = board_data_records[0]["column_order"]
+        column_order = [str(col_uuid) for col_uuid in column_order_uuid_list]
+
+        sorted_board_columns = sorted(board_columns, key=lambda column: column_order.index(column.get("column_uuid")))
+
+        board_data = {"column_order": column_order, "columns": sorted_board_columns}
+    else:
+        board_data = {"column_order": [], "columns": []}
+
+    return board_data
+
+
+@board_router.websocket("/ws/{board_uuid}")
+async def board_ws_endpoint(websocket: WebSocket, board_uuid: UUID4):
+    board_ws_manager = ConnectionManager(manager_id=board_uuid)
+    await board_ws_manager.connect(websocket)
+
+    # on connect, send existing column data
+    init_board_data = await get_board_data(board_uuid)
+    await board_ws_manager.send_json_data(init_board_data, websocket)
+
+    try:
+        while True:
+            data_dict = await websocket.receive_json()
+            crud_type = data_dict.get("crud")
+
+            if crud_type == "create":
+                column_create_data = data_dict.get("data")
+                new_column_order = column_create_data.get("column_order")
+
+                await create_column_and_update_board_column_order(
+                    ColumnCreate(**column_create_data.get("new_column")), new_column_order
+                )
+
+            if crud_type == "update":
+                column_update_data = data_dict.get("data")
+                new_column_order = column_update_data.get("column_order")
+
+                await update_column_and_update_board_column_order(
+                    Column(**column_update_data.get("updated_column")), new_column_order
+                )
+
+            if crud_type == "delete":
+                delete_column_uuid = data_dict.get("data").get("column_uuid")
+                new_column_order = data_dict.get("data").get("column_order")
+
+                await delete_column_and_update_board_column_order(board_uuid, delete_column_uuid, new_column_order)
+
+            if crud_type:
+                # update board data to everyone who's on the board
+                updated_board_data = await get_board_data(board_uuid)
+                await board_ws_manager.broadcast_json(updated_board_data)
+
+    except WebSocketDisconnect:
+        board_ws_manager.disconnect(websocket)
